f9f347a6c0134ec6dce06bb05664757a525d8be2
pouya <pouya.hp2004@yahoo.com>
2024-08-08 18:28:43 +0330

    Update: updated the backend

--- backend/routes/commentsRoutes.js
+++ backend/routes/commentsRoutes.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const SabzLearnShopDB = require("./../db/SabzLearnShop");
+const express = require('express');
+const SabzLearnShopDB = require('./../db/SabzLearnShop');
 
 const commentsRouter = express.Router();
 
 // routes
 
-commentsRouter.get("/", (req, res) => {
+commentsRouter.get('/', (req, res) => {
   let selectAllCommentsQuery = `SELECT Comments.id, Comments.isAccept , Comments.body, Comments.date, Comments.hour, Users.firsname as userID, Products.title as productID FROM Comments INNER JOIN Users ON Users.id = Comments.userID INNER JOIN Products ON Products.id = Comments.productID`;
 
   SabzLearnShopDB.query(selectAllCommentsQuery, (err, result) => {
@@ -17,7 +17,7 @@ commentsRouter.get("/", (req, res) => {
   });
 });
 
-commentsRouter.delete("/:commentID", (req, res) => {
+commentsRouter.delete('/:commentID', (req, res) => {
   let commentID = req.params.commentID;
 
   let deleteCommentQuery = `DELETE FROM Comments WHERE id = ${commentID}`;
@@ -30,9 +30,37 @@ commentsRouter.delete("/:commentID", (req, res) => {
   });
 });
 
-commentsRouter.put("/:commentID", (req, res) => {
+commentsRouter.put('/:commentID', (req, res) => {
   let commentID = req.params.commentID;
   let editCommentQuery = `UPDATE Comments SET body="${req.body.body}" WHERE id = ${commentID}`;
+
+  SabzLearnShopDB.query(editCommentQuery, (err, result) => {
+    if (err) {
+      res.send(null);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+commentsRouter.post('/accept/:commentID', (req, res) => {
+  let commentID = req.params.commentID;
+  console.log(commentID);
+  let editCommentQuery = `UPDATE Comments SET isAccept = 1 WHERE id = ${commentID}`;
+
+  SabzLearnShopDB.query(editCommentQuery, (err, result) => {
+    if (err) {
+      res.send(null);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+commentsRouter.post('/reject/:commentID', (req, res) => {
+  let commentID = req.params.commentID;
+  console.log(commentID);
+  let editCommentQuery = `UPDATE Comments SET isAccept = 0 WHERE id = ${commentID}`;
 
   SabzLearnShopDB.query(editCommentQuery, (err, result) => {
     if (err) {
